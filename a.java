89df226440f68f78188b621663c0b746fb679c3d
leoyfj <734085516@qq.com>
2014-05-23 12:29:01 +0800

    original

---
+++ a.java
@@ -0,0 +1,13 @@
+public class DBHelper{
+	public void insert(){
+		system.out.println("this is User A's  insert");
+	}
+
+	public void delete(){
+		system.out.println("this is public");
+	}
+	
+	public void hello(){
+		system.out.println("this is public");
+	}
+}
